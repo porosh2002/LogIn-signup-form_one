1bddb58bfef9dcb2994e5d69cb21bb3c6fd6eec4
porosh2002 <jamilkashem2002@gmail.com>
2020-12-01 03:28:08 +0600

    getting video

--- Client/src/Components/Dropzone/Dropjone.js
+++ Client/src/Components/Dropzone/Dropjone.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react'
-import Dropzone from 'react-dropzone';
+import React,{useState} from 'react'
 import { VscAdd} from "react-icons/vsc";
-const onDrop = ( files ) => {
-    console.log(files[0])
-}
-export default class Dropjone extends Component {
-    render() {
-        return (
-                <Dropzone 
-onDrop={onDrop}
-multiple={false}
-maxSize={800000000}>
-{({ getRootProps, getInputProps }) => (
-    <div className='DropDiv'
-        {...getRootProps()}
-    >
-        <input {...getInputProps()} />
-        <div className='icon-drop'><VscAdd /></div>
-
-    </div>
-)}
-</Dropzone>
-        )
-    }
+import {useDropzone} from 'react-dropzone'
+export default function Dropjone() {
+    const [yourImage,setImage]=useState([]);
+    const{getRootProps,getInputProps,isDragActive} = useDropzone({
+        accept:"video/*",
+        onDrop:(acceptedFiles)=>{
+            setImage(
+                acceptedFiles.map((upFile)=>Object.assign(upFile,{
+                    preview:URL.createObjectURL(upFile)
+                }))
+            )
+        }
+    })
+    return (
+<div>
+<div {...getRootProps()}>
+            <input {...getInputProps()}/>
+            {isDragActive ? <p>Drop Image Here</p> : <p>Select Image</p>}
+        </div>
+        {
+            yourImage.map((upFile,i)=>{
+                console.log(upFile)
+                return(
+                    <div key={i}>
+                        <video controls src={upFile.preview} />
+                    </div>
+                )
+            })
+        }
+</div>
+    )
 }
