09b1d339561f88c561841bd6dc7645c75d825e41
porosh2002 <jamilkashem2002@gmail.com>
2020-12-06 03:46:13 +0600

    account details menu added

--- Client/src/Components/Navigation/Navigation.js
+++ Client/src/Components/Navigation/Navigation.js
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import Logo from "../../Images/logo.png";
 import { IconWrap } from "../../Styled";
-import Search from '../Search/Search'
+import Search from "../Search/Search";
 import { Link } from "react-router-dom";
 import { VscAdd, VscBellDot, VscAccount } from "react-icons/vsc";
-export default function Navigation() {
-  return (
-<div className="navigation">
-      <Link to="/">
-        <img className="logo" src={Logo} alt="logo" />
-      </Link>
-      <Search />
-<nav style={{margin:"18px 0px",overflow:"hidden"}}>
-<IconWrap to="/upload">
-        <VscAdd />
-      </IconWrap>
-      <IconWrap to="#">
-        <VscBellDot />
-      </IconWrap>
-      <IconWrap to="#">
-        <VscAccount />
-      </IconWrap>
-</nav>
-<div className='NotificationDiv'>
-  <p className='NotificationMessage'>No navigation</p>
-</div>
-    </div>
-  );
+
+export default class Navigation extends PureComponent {
+  state = {
+    navOpen: false,
+    AccOpen:false,
+  };
+  navOpenCall = () => {
+    this.setState({ navOpen: !this.state.navOpen,AccOpen:false });
+  };
+  AccOpenCall = () => {
+    this.setState({ AccOpen: !this.state.AccOpen,navOpen:false });
+  };
+  CloseCall = () => {
+    this.setState({navOpen: false,AccOpen:false});
+  };
+  render() {
+    const { navOpen, AccOpen } = this.state;
+    const NavStyle = navOpen ? null : { display: "none" };
+    const AccStyle = AccOpen ? null : { display: "none" };
+    return (
+      <div className="navigation">
+        <Link onClick={this.navCloseCall} to="/">
+          <img className="logo" src={Logo} alt="logo" />
+        </Link>
+        <div style={{ marginTop: "17px" }} onClick={this.CloseCall}>
+          <Search />
+        </div>
+        <nav style={{ margin: "18px 0px", overflow: "hidden" }}>
+          <IconWrap onClick={this.CloseCall} to="/upload">
+            <VscAdd />
+          </IconWrap>
+          <IconWrap onClick={this.navOpenCall} to="#">
+            <VscBellDot />
+          </IconWrap>
+          <IconWrap onClick={this.AccOpenCall} to="#">
+            <VscAccount />
+          </IconWrap>
+        </nav>
+        <div style={NavStyle} className="NotificationDiv">
+          <p className="NotificationMessage">no navigation</p>
+        </div>
+        <div style={AccStyle} className="NotificationDiv">
+          <p className="NotificationMessage">Account details</p>
+        </div>
+      </div>
+    );
+  }
 }
--- Client/src/Components/Search/Search.js
+++ Client/src/Components/Search/Search.js
@@ -1,17 +1,17 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import {SearchBar,SearchButton} from '../../Styled';
 import { FaSearch} from "react-icons/fa";
-export class Search extends PureComponent {
-    render() {
-        return (
-            <div style={{display:"flex"}}>
-            <SearchBar />
-            <SearchButton>{<FaSearch />}</SearchButton>
-            </div>
-                
-        )
-    }
+
+function Search() {
+    return (
+        <div style={{display:"flex"}}>
+        <SearchBar />
+        <SearchButton>{<FaSearch />}</SearchButton>
+        </div>
+    )
 }
 
 export default Search
+
+
 
