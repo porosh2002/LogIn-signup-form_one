60df306951f0e193106198fb72490236fbea9f97
porosh2002 <jamilkashem2002@gmail.com>
2020-12-06 03:57:58 +0600

    footer re-created with style

--- Client/src/Footer.js
+++ Client/src/Footer.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className='FooterDiv'>
+            <div>© MD Jamil Kashem Porosh</div>
+            <div>email: jamilkashem@zoho.com</div>
+            <div><a href='https://www.facebook.com/mdjamilkashemporosh'>Facebook</a></div>
+            <div><a href='https://twitter.com/mdjamilkashem'>Twitter</a></div>
+            <div><a href='https://www.youtube.com/channel/UCyVcKvhhAi1enxen21JJzTw?view_as=subscriber'>Youtube</a></div>
+            <div><a href='https://github.com/porosh2002'>Github</a></div>
         </div>
     )
 }
