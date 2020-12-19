d755a56723f04e9c13ea93a01f11bd8355f07bfa
porosh2002 <jamilkashem2002@gmail.com>
2020-12-20 01:39:33 +0600

    Most View video updated

--- Client/src/Components/Video.js/VideoMostView.js
+++ Client/src/Components/Video.js/VideoMostView.js
@@ -1,12 +1,29 @@
-import React from 'react'
-// import VideoThumb from './videoThumb'
-// 21,43,30
-// 21,43,30
+import React from "react";
+import VideoThumb from "./videoThumb";
 export default function Video({ video }) {
-  const demoArray = [21, 43, 30];
-  demoArray.sort(function(a, b){return b-a})
+  const ViewsArray = [];
+  const ModifiedData = [];
+  video.map((data, i) => {
+    return ViewsArray.push(data.Views);
+  });
+  ViewsArray.sort(function (a, b) {
+    return b - a;
+  });
+  // Modifing Data
+  if (ModifiedData.length !== ViewsArray) {
+    for (var i = 0; i < ViewsArray.length; i++) {
+      for (var j = 0; j < ViewsArray.length; j++) {
+        if (ViewsArray[i] === video[j].Views) {
+          ModifiedData.push(video[j]);
+        }
+      }
+    }
+  }
   return (
     <div>
+      {ModifiedData.map((data, i) => {
+        return <VideoThumb VideoData={data} key={i} />;
+      })}
     </div>
-  )
+  );
 }
